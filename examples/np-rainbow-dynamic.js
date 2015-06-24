2d0edf9ad0c75cdd5fefd5d9b3820abc8bef4352
Ray Pierce <ray@digitalpierce.com>
2015-06-23 23:11:44 -0500

    Adding some rainbow color examples

---
+++ examples/np-rainbow-dynamic.js
@@ -0,0 +1,86 @@
+/**
+ * This example shows how to use node-pixel to cycle colors through your
+ * NeoPixel strip using Johnny-Five.
+ * https://vimeo.com/131618207
+ *
+ * created by @pierceray in June 2015
+ */
+var five = require("johnny-five");
+var pixel = require("../lib/pixel.js");
+
+var opts = {};
+opts.port = process.argv[2] || "";
+
+var board = new five.Board(opts);
+var strip = null;
+
+/**
+ * how many frames per second do you want to try?
+ *
+ * This number will fluctuate with the number of pixels that you are trying to
+ * update. With a NeoPixel Arduino Shield https://www.adafruit.com/product/1430
+ * 13 is the max fps I've gotten to work.  You milage may vary.
+ */
+var fps = 13;
+
+board.on("ready", function() {
+
+    console.log("Board ready, lets add light");
+
+    // setup the node-pixel strip.
+    strip = new pixel.Strip({
+        data: 6,
+        length: 40, // number of pixels in the strip.
+        board: this,
+        controller: "FIRMATA"
+    });
+
+    strip.on("ready", function() {
+        console.log("Strip ready, let's go");
+        dynamicRainbow(fps);
+    });
+
+    function dynamicRainbow( delay ){
+        console.log( 'dynamicRainbow' );
+
+        var showColor;
+        var cwi = 0; // colour wheel index (current position on colour wheel)
+        var foo = setInterval(function(){
+            if (++cwi > 255) {
+                cwi = 0;
+            }
+
+            for(var i = 0; i < strip.stripLength(); i++) {
+                showColor = colorWheel( ( cwi+i ) & 255 );
+                strip.pixel( i ).color( showColor );
+            }
+            strip.show();
+        }, 1000/delay);
+    }
+
+    // Input a value 0 to 255 to get a color value.
+    // The colors are a transition r - g - b - back to r.
+    function colorWheel( WheelPos ){
+        var r,g,b;
+        WheelPos = 255 - WheelPos;
+
+        if ( WheelPos < 85 ) {
+            r = 255 - WheelPos * 3;
+            g = 0;
+            b = WheelPos * 3;
+        } else if (WheelPos < 170) {
+            WheelPos -= 85;
+            r = 0;
+            g = WheelPos * 3;
+            b = 255 - WheelPos * 3;
+        } else {
+            WheelPos -= 170;
+            r = WheelPos * 3;
+            g = 255 - WheelPos * 3;
+            b = 0;
+        }
+        // returns a string with the rgb value to be used as the parameter
+        return "rgb(" + r +"," + g + "," + b + ")";
+    }
+
+});
