e0ef294d06285d515919922fad5ffae79539ea71
ajfisher <ajfisher.td@gmail.com>
2015-08-17 13:59:16 +1000

    First pass pixel level tests

--- test/core.js
+++ test/core.js
@@ -104,7 +104,7 @@ exports["Strip - Firmata"] = {
             };
 
             strip.color("#FFFFFF");
-            test.deepEqual(strip.pixel(1).color(), colourcheck, "Check colours set");
+            test.deepEqual(strip.pixel(0).color(), colourcheck, "Check colours set");
 
             colourcheck = {
                 r: 255, g: 0, b: 0,
@@ -124,7 +124,7 @@ exports["Strip - Firmata"] = {
             };
             
             strip.color("blue");
-            test.deepEqual(strip.pixel(1).color(), colourcheck, "Colour set using name");
+            test.deepEqual(strip.pixel(2).color(), colourcheck, "Colour set using name");
             
             colourcheck = {
                 r: 0, g: 255, b: 0,
@@ -134,7 +134,7 @@ exports["Strip - Firmata"] = {
             };
             
             strip.color([0, 255, 0]);
-            test.deepEqual(strip.pixel(1).color(), colourcheck, "Colour set using options");
+            test.deepEqual(strip.pixel(3).color(), colourcheck, "Colour set using options");
 
 
             test.done();
@@ -143,6 +143,89 @@ exports["Strip - Firmata"] = {
     },
 
 }
+
+
+exports["Pixel - Firmata"] = {
+    setUp: function(done){
+        
+        this.write = sinon.stub(MockSerialPort.prototype, "write", function(buffer, callback) {
+            if (typeof callback === "function") {
+                process.nextTick(callback);
+            } else {
+                return;
+            }
+        });
+
+        this.board = newBoard();
+
+        this.strip = new pixel.Strip({
+            data: 6,
+            length: 4,
+            board: this.board,
+            controller: "FIRMATA",
+        });
+
+        done();
+    },
+
+    tearDown: function(done) {
+        Board.purge();
+        restore(this);
+        done();
+    },
+
+    pixelColour: function(test) {
+        // tests if the colour sequences are working okay
+        test.expect(4);
+
+
+            var colourcheck = {
+                r: 255, g: 255, b: 255,
+                hexcode: "#FFFFFF",
+                color: "white",
+                rgb: [255, 255, 255],
+            };
+
+            this.strip.pixel(0).color("#FFFFFF");
+            test.deepEqual(this.strip.pixel(0).color(), colourcheck, "Check colours set");
+
+            colourcheck = {
+                r: 255, g: 0, b: 0,
+                hexcode: "#FF0000",
+                color: "red",
+                rgb: [255, 0, 0],
+            };
+
+            this.strip.pixel(1).color("#F00");
+            test.deepEqual(this.strip.pixel(1).color(), colourcheck, "Check colours updated");
+
+            colourcheck = {
+                r: 0, g: 0, b: 255,
+                hexcode: "#0000FF",
+                color: "blue",
+                rgb: [0, 0, 255],
+            };
+            
+            this.strip.pixel(2).color("blue");
+            test.deepEqual(this.strip.pixel(2).color(), colourcheck, "Colour set using name");
+            
+            colourcheck = {
+                r: 0, g: 255, b: 0,
+                hexcode: "#00FF00",
+                color: "lime",
+                rgb: [0, 255, 0],
+            };
+            
+            //FIXME This is incorrect and needs to be linked the right way.
+            this.strip.pixel.color([0, 255, 0]);
+            test.deepEqual(this.strip.pixel(3).color(), colourcheck, "Colour set using options");
+
+            test.done();
+
+    },
+
+}
+
 
 
 
