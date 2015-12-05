f3c9c954d518c67cb75a441b58af5a9323032094
ajfisher <ajfisher.td@gmail.com>
2015-12-05 21:30:05 +1100

    set pixel initial multi strip detection in place

--- firmware/src/libs/ws2812/ws2812.cpp
+++ firmware/src/libs/ws2812/ws2812.cpp
@@ -7,12 +7,14 @@ WS2812 strips[MAX_STRIPS];
 
 uint8_t no_strips = 0; // how many strips have we configured.
 uint16_t strip_lengths[MAX_STRIPS]; // now long each strip is.
+bool strip_changed[MAX_STRIPS]; // used to optimise strip writes.
 
 void ws2812_initialise() {
     // initialises the strip defaults.
 
     for (uint8_t i = 0; i < MAX_STRIPS; i++) {
         strip_lengths[i] = 0;
+        strip_changed[i] = false;
     }
 }
 
@@ -37,9 +39,10 @@ void process_command(byte argc, byte *argv){
         case PIXEL_SHOW: {
             // FIXME update this to use strips and iterate over it.
             for (uint8_t i = 0; i< MAX_STRIPS; i++) {
-                if (strips[i].get_length() > 0) {
+                if (strips[i].get_length() > 0 && strip_changed[i]) {
                     strips[i].sync();
                 }
+                strip_changed[i] = false;
             }
             break;
         }
@@ -49,10 +52,11 @@ void process_command(byte argc, byte *argv){
             uint32_t strip_colour = (uint32_t)argv[1] + ((uint32_t)argv[2]<<7) + ((uint32_t)argv[3]<<14) + ((uint32_t)argv[4] << 21);
             for (uint8_t i = 0; i < MAX_STRIPS; i++) {
                 if (strips[i].get_length() > 0) {
-            // TODO put detection in for colour off and then do a memset on it.
+                    // TODO put detection in for colour off and then do a memset on it.
                     for (uint16_t j = 0; j<strips[i].get_length(); j++) {
                         strips[i].set_rgb_at(j, strip_colour);
                     }
+                    strip_changed[i] = true;
                 }
             }
             break;
@@ -61,8 +65,25 @@ void process_command(byte argc, byte *argv){
             // sets the pixel given by the index to the given colour
             uint16_t index = (uint16_t)argv[1] + ((uint16_t)argv[2]<<7);
             uint32_t colour = (uint32_t)argv[3] + ((uint32_t)argv[4]<<7) + ((uint32_t)argv[5]<<14) + ((uint32_t)argv[6] << 21);
-            strips[0].set_rgb_at(index, colour);
-            strips[1].set_rgb_at(index, colour);
+
+            int8_t strip = 0;
+            for (uint8_t i = 0; i < MAX_STRIPS; i++) {
+                if (strip_lengths[i] == 0) {
+                    // we are outside of the number of pixels we have so break
+                    break;
+                }
+                if (index < strip_lengths[i]) {
+                    strip = i;
+                    break;
+                }
+            }
+
+            if (strip >= 0) {
+                strips[strip].set_rgb_at(index, colour);
+                strip_changed[strip] = true;
+            }
+            //strips[1].set_rgb_at(index, colour);
+            //strip_changed[1] = false;
             // FIXME this needs to figure out which strip to use and set it.
             break;
         }
