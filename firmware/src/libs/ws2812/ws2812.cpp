3f7f885e555cf94de3f286ceb9dab8020e592e1b
ajfisher <ajfisher.td@gmail.com>
2016-09-10 21:39:05 +1000

    fixes to make better selections on dirty strips properly

--- firmware/src/libs/ws2812/ws2812.cpp
+++ firmware/src/libs/ws2812/ws2812.cpp
@@ -10,6 +10,7 @@ bool strip_changed[MAX_STRIPS]; // used to optimise strip writes.
 
 uint8_t *px;
 uint16_t px_count;
+uint8_t strip_count = 0; // number of strips being used.
 uint8_t color_depth = 3; // Bytes used to hold one pixel
 
 uint8_t offsetRed;
@@ -56,6 +57,8 @@ void initialise_pixels(uint16_t num_pixels) {
     }
 #if DEBUG
     Serial2.print("Initialising ");
+    Serial2.print(strip_count);
+    Serial2.print(" strips ");
     Serial2.print(px_count);
     Serial2.println(" pixels");
 #endif
@@ -71,16 +74,6 @@ uint8_t set_rgb_at(uint16_t index, uint32_t px_value) {
         px[OFFSET_G(tmp_pixel)] = (uint8_t)(px_value >> 8);
         px[OFFSET_B(tmp_pixel)] = (uint8_t)px_value;
 
-#if DEBUG
-/**        Serial2.print("Set RGB: ");
-        Serial2.print(index);
-        Serial2.print(" ");
-        Serial2.print(px[OFFSET_R(tmp_pixel)]);
-        Serial2.print(" ");
-        Serial2.print(px[OFFSET_G(tmp_pixel)]);
-        Serial2.print(" ");
-        Serial2.println(px[OFFSET_B(tmp_pixel)]);**/
-#endif
         return 0;
     }
     return 1;
@@ -90,17 +83,13 @@ void process_command(byte argc, byte *argv){
     // this takes a pixel command that has been determined and then
     // processes it appropriately.
 
-    Serial2.print("Reciving command ");
     uint8_t command = 0xF & argv[0];
-    Serial2.println(command);
-
 
     // now process the command.
     switch (command) {
         case PIXEL_SHOW: {
             // iterate over the strips and show those required.
 
-            print_pixels();
             if (! writingFrame) {
                 writingFrame = true;
                 for (uint8_t i = 0; i< MAX_STRIPS; i++) {
@@ -115,7 +104,6 @@ void process_command(byte argc, byte *argv){
         }
         case PIXEL_SET_STRIP: {
             // sets the entirety of the strip to one colour
-            Serial2.println("Setting strip");
 
             uint32_t strip_colour = (uint32_t)argv[1] +
                 ((uint32_t)argv[2]<<7) +
@@ -123,34 +111,16 @@ void process_command(byte argc, byte *argv){
                 ((uint32_t)argv[4] << 21);
 
             if (strip_colour == 0) {
-                // TODO: set this correctly
+                // set all of the pixels back to 0
                 memset(px, 0, px_count * color_depth);
             } else {
                 for (uint16_t i = 0; i < px_count; i++) {
                     set_rgb_at(i, strip_colour);
                 }
-                // set all the strips dirty for update
-                for (uint8_t j = 0; j < MAX_STRIPS; j++) {
-                    if (strips[j].get_length() > 0) {
-                        strip_changed[j] = true;
-                    }
-                }
             }
+            // set all the strips dirty for update
+            memset(strip_changed, true, strip_count);
 
-/**            for (uint8_t i = 0; i < MAX_STRIPS; i++) {
-                if (strips[i].get_length() > 0) {
-                    // If this is a blank then call strip off which is a bit more efficient
-                    if (strip_colour == 0) {
-                        strips[i].set_off();
-                    } else {
-                        for (uint16_t j = 0; j<strips[i].get_length(); j++) {
-                            //strips[i].set_rgb_at(j, strip_colour);
-                            set_rgb_at(j, strip_colour);
-                        }
-                    }
-                    strip_changed[i] = true;
-                }
-            }**/
             break;
         }
         case PIXEL_SET_PIXEL: {
@@ -159,35 +129,20 @@ void process_command(byte argc, byte *argv){
             uint32_t colour = (uint32_t)argv[3] + ((uint32_t)argv[4]<<7) +
                 ((uint32_t)argv[5]<<14) + ((uint32_t)argv[6] << 21);
 
-            int8_t strip = -1;
-
             set_rgb_at(index, colour);
 
             // TODO redo how we do this detection and how we mark a strip
             // as being dirty and needing an update
-            for (uint8_t i = 0; i < MAX_STRIPS; i++) {
-                /**if (strip_lengths[i] == 0) {
-                    // we are outside of the number of pixels we have so break
-                    break;
-                }**/
+            for (uint8_t i = 0; i < strip_count; i++) {
+                // find the strip where this pixel is located and
+                // then mark it dirty - but then bail out so you don't
+                // overprocess anything else.
                 if (index < strip_lengths[i]) {
-                    strip = i;
+                    strip_changed[i] = true;
                     break;
                 }
             }
 
-            if (strip >= 0) {
-                if (strip == 0) {
-                    //strips[strip].set_rgb_at(index, colour);
-                    ;
-                } else {
-                    // calculate the index value for the strip we're actually
-                    // looking at relative to the overall strip details
-                    //strips[strip].set_rgb_at(index-strip_lengths[strip-1], colour);
-                    ;
-                }
-                strip_changed[strip] = true;
-            }
             break;
         }
         case PIXEL_CONFIG: {
@@ -210,18 +165,14 @@ void process_command(byte argc, byte *argv){
 
                     // get the top two bits for the colour order type.
                     uint8_t colour_type = (uint8_t)argv[argv_offset+1]>>5;
-                    // TODO: Rip out the strip versions below
                     switch (colour_type) {
                         case PIXEL_COLOUR_GRB:
-                            //strips[i].setColorOrderGRB();
                             setColorOrderGRB();
                             break;
                         case PIXEL_COLOUR_RGB:
-                            //strips[i].setColorOrderRGB();
                             setColorOrderRGB();
                             break;
                         case PIXEL_COLOUR_BRG:
-                            //strips[i].setColorOrderBRG();
                             setColorOrderBRG();
                             break;
                     }
@@ -235,8 +186,10 @@ void process_command(byte argc, byte *argv){
                     strip_lengths[i] = strips[i].get_length() + prev_strip_length;
                     // set the strip's offset so it knows where it is in the
                     // 1D pixel array
-                    strips[i].set_offset(strip_lengths[i]);
+                    strips[i].set_offset(prev_strip_length);
                     px_count = px_count + strips[i].get_length();
+
+                    strip_count++;
                 }
                 // now initialise our pixel count
                 initialise_pixels(px_count);
