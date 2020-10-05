1b8cd446212c6cf00813a6e63f3b59c2f5be6915
ajfisher <ajfisher.td@gmail.com>
2020-10-05 11:27:01 +1100

    fix(linting): Updated examples to use standard linting rules

--- examples/firmata.js
+++ examples/firmata.js
@@ -1,43 +1,41 @@
 // This example shows how to use node-pixel using firmata as the
 // hook for the board.
 
-var firmata = require("firmata");
-var pixel = require("../lib/pixel.js");
+const firmata = require('firmata');
+const pixel = require('../lib/pixel.js');
 
-var opts = {};
+const opts = {};
 if (process.argv[2] == undefined) {
-    console.log("Please supply a device port to connect to");
-    process.exit();
+  console.log('Please supply a device port to connect to');
+  process.exit();
 }
 
 opts.port = process.argv[2];
 
-var strip = null;
+let strip = null;
 
 var board = new firmata.Board(opts.port, function() {
+  console.log('Firmata ready, lets add light');
 
-    console.log("Firmata ready, lets add light");
+  strip = new pixel.Strip({
+    data: 6,
+    length: 4,
+    firmata: board
+  });
 
-    strip = new pixel.Strip({
-        data: 6,
-        length: 4,
-        firmata: board,
-    });
+  let pos = 0;
+  const colors = ['red', 'green', 'blue', 'yellow', 'cyan', 'magenta', 'white'];
+  let current_color = 0;
 
-    var pos = 0;
-    var colors = ["red", "green", "blue", "yellow", "cyan", "magenta", "white"];
-    var current_color = 0;
+  const blinker = setInterval(function() {
+    strip.color('#000'); // blanks it out
 
-    var blinker = setInterval(function() {
+    if (++pos >= strip.length) {
+      pos = 0;
+      if (++current_color>= colors.length) current_color = 0;
+    }
+    strip.pixel(pos).color(colors[current_color]);
 
-        strip.color("#000"); // blanks it out
-
-        if (++pos >= strip.length) {
-            pos = 0;
-            if (++current_color>= colors.length) current_color = 0;
-        }
-        strip.pixel(pos).color(colors[current_color]);
-
-        strip.show();
-    }, 1000/2);
+    strip.show();
+  }, 1000/2);
 });
--- examples/johnnyfive.js
+++ examples/johnnyfive.js
@@ -1,45 +1,42 @@
 // This example shows how to use node-pixel using Johnny Five as the
 // hook for the board.
-var five = require("johnny-five");
-var pixel = require("../lib/pixel.js");
+const five = require('johnny-five');
+const pixel = require('../lib/pixel.js');
 
-var opts = {};
-opts.port = process.argv[2] || "";
+const opts = {};
+opts.port = process.argv[2] || '';
 
-var board = new five.Board(opts);
-var strip = null;
+const board = new five.Board(opts);
+let strip = null;
 
-var fps = 10; // how many frames per second do you want to try?
+const fps = 10; // how many frames per second do you want to try?
 
-board.on("ready", function() {
+board.on('ready', function() {
+  console.log('Board ready, lets add light');
 
-    console.log("Board ready, lets add light");
+  strip = new pixel.Strip({
+    data: 6,
+    length: 8,
+    color_order: pixel.COLOR_ORDER.GRB,
+    board: this,
+    controller: 'FIRMATA'
+  });
 
-    strip = new pixel.Strip({
-        data: 6,
-        length: 8,
-        color_order: pixel.COLOR_ORDER.GRB,
-        board: this,
-        controller: "FIRMATA",
+  strip.on('ready', function() {
+    console.log("Strip ready, let's go");
+
+    const colors = ['red', 'green', 'blue', 'yellow', 'cyan', 'magenta', 'white'];
+    // var current_colors = [0,1,2,3,4];
+    const current_pos = [0,1,2,3,4];
+
+    current_pos.forEach((pos) => {
+      strip.pixel(pos).color(colors[pos]);
     });
 
-    strip.on("ready", function() {
+    const blinker = setInterval(function() {
+      strip.shift(1, pixel.FORWARD, true);
 
-        console.log("Strip ready, let's go");
-
-        var colors = ["red", "green", "blue", "yellow", "cyan", "magenta", "white"];
-        //var current_colors = [0,1,2,3,4];
-        var current_pos = [0,1,2,3,4];
-
-        current_pos.forEach((pos) => {
-            strip.pixel(pos).color(colors[pos]);
-        });
-
-        var blinker = setInterval(function() {
-
-            strip.shift(1, pixel.FORWARD, true);
-
-            strip.show();
-        }, 1000/fps);
-    });
+      strip.show();
+    }, 1000/fps);
+  });
 });
